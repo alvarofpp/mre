c0e468b1e77d48bb955fa09e6444c87880ba4f61
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2019-10-04 20:29:05 -0300

    Corrections to Regex and Comment class behavior

--- mre/Comment.py
+++ mre/Comment.py
@@ -1,11 +1,13 @@
 class Comment:
-    """Comment Class."""
+    """Comment class."""
 
     def __init__(self, raw_comment: str = ""):
         self.raw_comment = raw_comment
 
-    def set(self, raw_comment: str = "") -> 'Comment':
-        self.raw_comment = raw_comment
-
     def get(self) -> str:
-        return self.raw_comment
+        """Return regex."""
+        return "(?#{})".format(self.raw_comment)
+
+    def set(self, raw_comment: str = ""):
+        """Set regex value."""
+        self.raw_comment = raw_comment
