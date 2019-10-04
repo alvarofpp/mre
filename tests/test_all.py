8c5ccea2d3e11c7e98d23eec4daaf96583872395
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2019-10-04 20:29:15 -0300

    Comment tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -4,6 +4,7 @@ import tests.test_Set
 import tests.test_Regex
 import tests.test_Group
 import tests.test_Anchor
+import tests.test_Comment
 import tests.test_Quantifier
 import tests.test_Helper_Range
 
@@ -16,6 +17,7 @@ def suite():
     suite.addTests(loader.loadTestsFromModule(tests.test_Regex))
     suite.addTests(loader.loadTestsFromModule(tests.test_Group))
     suite.addTests(loader.loadTestsFromModule(tests.test_Anchor))
+    suite.addTests(loader.loadTestsFromModule(tests.test_Comment))
     suite.addTests(loader.loadTestsFromModule(tests.test_Quantifier))
     suite.addTests(loader.loadTestsFromModule(tests.test_Helper_Range))
 
