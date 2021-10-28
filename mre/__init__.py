70c3b8b3e81df07c1d196844c195c978567803b5
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-10-28 15:53:44 -0300

    refactor: init

--- mre/__init__.py
+++ mre/__init__.py
@@ -1,7 +1,10 @@
+# From Regex
 from .Regex import Regex
-from .Quantifier import Quantifier
-from .Set import Set
-from .Group import Group
 from .Anchor import Anchor
 from .Comment import Comment
+from .Quantifier import Quantifier
+from .Set import Set
+
+# From Group
+from .Group import Group
 from .Lookaround import Lookahead
